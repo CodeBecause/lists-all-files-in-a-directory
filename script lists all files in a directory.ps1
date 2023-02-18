e2f9294d66b824f8be491cf1939109fb22434110
CodeBecause <116619103+CodeBecause@users.noreply.github.com>
2023-02-18 10:38:48 -0500

    Add files via upload

---
+++ script lists all files in a directory.ps1
@@ -0,0 +1,13 @@
+﻿# This script lists all files in a directory
+
+# Set the directory path
+$dir = "C:\Users\Username\Documents"
+
+# Get a list of all files in the directory
+$files = Get-ChildItem $dir
+
+# Display the list of files
+Write-Host "Files in $dir:"
+foreach ($file in $files) {
+    Write-Host $file.Name
+}
